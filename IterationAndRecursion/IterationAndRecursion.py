75b8b15667f41f4562b815cfa409f02fa50e1c63
username <useremail>
2023-07-20 17:47:59 -0700

    added iterative and recursive codes

--- IterationAndRecursion/IterationAndRecursion.py
+++ IterationAndRecursion/IterationAndRecursion.py
@@ -1,2 +1,16 @@
 #KalvinBlowe #CIS261 #IterationAndRecursion
+#iteration
+def factorial_iterative_while(n):
+    result = 1
+    while n >= 1:
+        result *= n
+        n -= 1
+    return result 
+#recursion
+def factorial(n):
+    if n <= 1:
+        return 1
+    else:
+        return n * factorial(n-1)
+
 
